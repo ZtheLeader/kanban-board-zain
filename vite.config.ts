09925cd339b44587b4f0b49945816ac8b8b9f02c
ztheleader <ztheleader@gmail.com>
2025-08-09 18:34:27 +0200

    chore: update testing setup with vitest configuration and add setupTests file

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     ...configDefaults,
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    setupFiles: './src/utils/setupTests.ts',
   },
 });

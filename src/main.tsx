98c962d0017e14217c03cdd6a70e842f6ae790b5
ztheleader <ztheleader@gmail.com>
2025-08-10 21:11:32 +0200

    fix: replace BrowserRouter with HashRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css'
 import App from './App.tsx'
@@ -8,10 +8,10 @@ import { KanbanProvider } from './context/kanban/KanbanContextProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <KanbanProvider>
         <App />
       </KanbanProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
